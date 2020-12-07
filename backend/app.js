1ccacabf7ed92539cc3ff12cdab500f1f0045363
mganderson86 <mganderson2@uh.edu>
2020-12-07 09:48:12 -0600

    backend demo

--- backend/app.js
+++ backend/app.js
@@ -61,6 +61,28 @@ app.put("/UpdateCATAnswer/:id?", function (req, res) {
 	});
 });
 
+app.put("/InsertStudentInformation/", function (req, res) {
+	const values = req.body.values;
+	
+	mssql.connect(config, function (err) {
+		if (err) {
+			return callback(err);
+        } else 
+      	var request = new mssql.Request();
+        values.map((value, key) => {
+            request.input({key}, sql.VarChar, {value})
+        });
+        
+        request.execute("dbo.InsertStudentInformation", function (err, result) {
+			if (err) {
+				console.log("dbo.InsertStudentInformation: " + err);
+			}
+			let str = JSON.stringify(result);
+			res.send(str); //send to front_end
+		});
+	});
+});
+
 require("./routes/nextQuestion")(app);
 
 // app.get("/", function (req, res) {
