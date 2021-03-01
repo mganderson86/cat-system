0f580d678ebe437a1284840b39de14bd2d773717
mganderson86 <74729738+mganderson86@users.noreply.github.com>
2021-03-01 07:42:03 -0600

    Delete db.config.js

--- backend/db.config.js
+++
@@ -1,7 +0,0 @@
-// module.exports = {
-//     user     : 'test',
-//     host: 'http://hbs-310s-it-xxx.times.uh.edu', 
-//     password : '1234pass!',
-//     database : 'CAT',
-//     port     : '1433'
-// }
